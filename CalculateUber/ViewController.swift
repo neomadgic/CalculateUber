ad7dbdb5172f977d23b0f0dcd4df5d7add4cd7ae
Vu Dang <jublusion5@gmail.com>
2017-01-15 00:31:08 -0600

    Implemented CLLocation and MapKit. Allowed locations in plist

--- CalculateUber/ViewController.swift
+++ CalculateUber/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var pickupTimeLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    var currentLocation = CLLocation()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setCurrentLocation()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        setCurrentLocation()
     }
-
-
+    
+    @IBAction func onCalculatePressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onRequestUberPressed(_ sender: Any) {
+        
+    }
+    
+    func setCurrentLocation() {
+        
+        guard locationManager.location != nil else {
+            print("Can't find location")
+            printErrorAlert()
+            return
+        }
+        currentLocation = locationManager.location!
+        print(currentLocation.coordinate)
+    }
+    
+    func printErrorAlert() {
+        
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable Location Services in Settings in order for CalculateUber to set your starting location", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        mapView.showsUserLocation = (status == .authorizedAlways)
+    }
+    
+}
